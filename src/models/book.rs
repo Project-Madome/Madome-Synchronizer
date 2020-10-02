c4758fa32af6fc087e9ad576bdbbdb94c4f289c5
Lee Tae Woo <syr@meu.works>
2020-10-02 16:39:45 +0900

    change Book struct

--- src/models/book.rs
+++ src/models/book.rs
@@ -2,14 +2,14 @@ use super::Metadata;
 
 #[derive(Debug, PartialEq)]
 pub struct Book {
-    pub id: Metadata,
-    pub title: Metadata,
-    pub groups: Metadata,
-    pub artists: Metadata,
-    pub series: Metadata,
-    pub tags: Metadata,
-    pub language: Metadata,
-    pub content_type: Metadata,
-    pub characters: Metadata,
-    pub created_at: Metadata,
+    pub id: i32,
+    pub title: String,
+    pub groups: Vec<String>,
+    pub artists: Vec<String>,
+    pub series: Vec<String>,
+    pub tags: Vec<String>,
+    pub characters: Vec<String>,
+    pub language: String,
+    pub content_type: String,
+    pub created_at: String,
 }
