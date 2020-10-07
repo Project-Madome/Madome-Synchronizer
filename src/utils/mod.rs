da68f5342863a71fcdec1ca4e0023b9d3846b3b3
Lee Tae Woo <syr@meu.works>
2020-10-07 18:55:28 +0900

    Add futureUtil trait

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,22 @@
+use anyhow;
+use async_trait::async_trait;
+
 mod await_futures;
 
 pub use await_futures::{await_futures, PinFuture};
+
+#[async_trait]
+pub trait FutureUtil {
+    type Item;
+
+    async fn await_futures(self, concurrency_limit: usize) -> anyhow::Result<Vec<Self::Item>>;
+}
+
+#[async_trait]
+impl<T: Send + 'static> FutureUtil for Vec<PinFuture<T>> {
+    type Item = T;
+
+    async fn await_futures(self, concurrency_limit: usize) -> anyhow::Result<Vec<Self::Item>> {
+        await_futures(self, concurrency_limit).await
+    }
+}
