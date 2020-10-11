bd130a1d642a6a7538913e415e1ef4752f79fed2
Lee Tae Woo <syr@meu.works>
2020-10-11 11:17:13 +0900

    Change id type

--- src/parser/nozomi.rs
+++ src/parser/nozomi.rs
@@ -3,9 +3,9 @@ use std::convert::TryInto;
 use anyhow;
 use async_trait::async_trait;
 use bytes::Bytes;
+use log::{debug, trace};
+use madome_client::book::Language;
 use reqwest;
-
-use crate::models::Language;
 
 use super::Parser;
 
@@ -34,7 +34,7 @@ impl Nozomi {
 #[async_trait]
 impl Parser for Nozomi {
     type RequestData = Bytes;
-    type ParseData = Vec<i32>;
+    type ParseData = Vec<u32>;
 
     fn request_data(&self) -> anyhow::Result<&Box<Self::RequestData>> {
         match self.request_data {
@@ -51,10 +51,14 @@ impl Parser for Nozomi {
     }
 
     async fn request(mut self) -> anyhow::Result<Box<Self>> {
+        trace!("Nozomi::request()");
         let client = reqwest::Client::builder().build()?;
 
         let start_bytes = (self.page - 1) * self.per_page * 4;
         let end_bytes = start_bytes + self.per_page * 4 - 1;
+
+        debug!("start_bytes = {}", start_bytes);
+        debug!("end_bytes = {}", end_bytes);
 
         let bytes = client
             .get(self.url().await?.as_str())
@@ -69,22 +73,25 @@ impl Parser for Nozomi {
     }
 
     async fn parse(&self) -> anyhow::Result<Self::ParseData> {
+        trace!("Nozomi::parse()");
         let request_data = self.request_data()?;
 
         let mut res = vec![];
 
         'a: for i in (0..request_data.len()).step_by(4) {
-            let mut temp: i32 = 0;
+            let mut temp: u32 = 0;
 
             for j in 0..3 {
                 // https://github.com/Project-Madome/Madome-Synchronizer/issues/1
                 // temp += TryInto::<i32>::try_into(request_data[i + (3 - j)])? << (j << 3);
                 if let Some(a) = request_data.get(i + (3 - j)) {
-                    temp += TryInto::<i32>::try_into(*a)? << (j << 3);
+                    temp += TryInto::<u32>::try_into(*a)? << (j << 3);
                 } else {
                     break 'a;
                 }
             }
+
+            debug!("id = {}", temp);
 
             res.push(temp);
         }
@@ -97,7 +104,7 @@ impl Parser for Nozomi {
 
 #[cfg(test)]
 mod test {
-    use crate::models::Language;
+    use madome_client::book::Language;
 
     use super::Nozomi;
     use super::Parser;
