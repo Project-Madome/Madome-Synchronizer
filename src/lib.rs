3d03aef8ffced441f0829bcb1f97c6894b633622
Lee Tae Woo <syr@meu.works>
2020-10-19 23:34:05 +0900

    Change stage module

--- src/lib.rs
+++ src/lib.rs
@@ -3,59 +3,39 @@ pub mod parser;
 pub mod utils;
 
 pub mod stage {
+
     use log::{error, info};
-    use madome_client::book::Book;
+
+    pub fn update(id: u32, stage: Stage) -> anyhow::Result<()> {
+        match stage {
+            Stage::Fail => error!("{}: {}", id, stage.to_string()),
+            other => info!("{}: {}", id, other.to_string()),
+        }
+
+        Ok(())
+    }
 
     pub enum Stage {
-        Download,
-        Upload,
+        ParsedBook,
+        ParsedImages,
+        AddedThumbnail,
+        AddedImages,
+        AddedBook,
+        Fail,
     }
 
-    pub struct ParseStage;
+    impl ToString for Stage {
+        fn to_string(&self) -> String {
+            let r = match self {
+                Self::ParsedBook => "Parsed Book",
+                Self::ParsedImages => "Parsed Images",
+                Self::AddedThumbnail => "Added Thumnail",
+                Self::AddedImages => "Added Images",
+                Self::AddedBook => "Added Book",
+                Self::Fail => "Fail",
+            };
 
-    impl ParseStage {
-        pub fn update(r: &anyhow::Result<Book>) {
-            match r {
-                Ok(book) => info!("parse finish\nid = {}\ntitle = {}", book.id, book.title),
-                Err(err) => error!("parse error\n{:?}", err),
-            }
+            r.to_string()
         }
-    }
-
-    type DownloadStageInner = (u32, usize, String);
-    pub struct DownloadStage;
-
-    impl DownloadStage {
-        pub fn update(r: &anyhow::Result<DownloadStageInner>) {
-            match r {
-                Ok((id, current_page, ext)) => info!(
-                    "download finish\nid = {}\npage = {}\next = {}",
-                    id, current_page, ext
-                ),
-                Err(err) => error!("download error\n{:?}", err),
-            }
-        }
-    }
-
-    type URLPath = String;
-    type UploadStageInner<'a> = (URLPath, usize);
-    pub struct UploadStage;
-
-    impl UploadStage {
-        pub fn update(r: &anyhow::Result<UploadStageInner>) {
-            match r {
-                Ok((url_path, current_page)) => info!(
-                    "upload finish\nurl_path = {}\npage = {}",
-                    url_path, current_page
-                ),
-                Err(err) => error!("upload error\n{:?}", err),
-            }
-        }
-    }
-
-    pub struct CompleteStage;
-
-    impl CompleteStage {
-        pub fn update(_r: &anyhow::Result<()>) {}
     }
 }
