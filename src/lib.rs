640ff63b2985b967eb37f46cdfc82b4184e2fee3
Lee Tae Woo <syr@meu.works>
2020-10-20 19:24:56 +0900

    change Stage::Fail(Error)

--- src/lib.rs
+++ src/lib.rs
@@ -8,23 +8,23 @@ pub mod stage {
 
     pub fn update(id: u32, stage: Stage) -> anyhow::Result<()> {
         match stage {
-            Stage::Fail => error!("{}: {}", id, stage.to_string()),
+            Stage::Fail(err) => error!("{}: {}: {:#?}", id, stage.to_string(), err),
             other => info!("{}: {}", id, other.to_string()),
         }
 
         Ok(())
     }
 
-    pub enum Stage {
+    pub enum Stage<'a> {
         ParsedBook,
         ParsedImages,
         AddedThumbnail,
         AddedImages,
         AddedBook,
-        Fail,
+        Fail(&'a anyhow::Error),
     }
 
-    impl ToString for Stage {
+    impl<'a> ToString for Stage<'a> {
         fn to_string(&self) -> String {
             let r = match self {
                 Self::ParsedBook => "Parsed Book",
@@ -32,7 +32,7 @@ pub mod stage {
                 Self::AddedThumbnail => "Added Thumnail",
                 Self::AddedImages => "Added Images",
                 Self::AddedBook => "Added Book",
-                Self::Fail => "Fail",
+                Self::Fail(_) => "Fail",
             };
 
             r.to_string()
